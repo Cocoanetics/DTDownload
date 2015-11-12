b0ec91b1f02d7c7e71e1833e0d04a87333c325ca
Oliver Drobnik <oliver@cocoanetics.com>
2015-11-12 12:07:08 +0100

    Made custom module map for custom umbrella header for DTDownload

---
+++ Core/DTDownload-Umbrella.h
@@ -0,0 +1,18 @@
+//
+//  DTDownload-Umbrella.h
+//  DTDownload
+//
+//  Created by Oliver Drobnik on 12/11/15.
+//  Copyright © 2015 Drobnik KG. All rights reserved.
+//
+
+#ifndef DTDownload_Umbrella_h
+#define DTDownload_Umbrella_h
+
+#import "DTDownload.h"
+#import "DTDownloadCache.h"
+#import "DTCachedFile.h"
+#import "DTDownloadQueue.h"
+#import "DTDownloadItem.h"
+
+#endif /* DTDownload_Umbrella_h */
